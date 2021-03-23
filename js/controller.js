9344441d6c6674c6b6f5c63e98d7993807541987
John-Kodes <jdanielsemine@gmail.com>
2021-03-23 19:51:01 +0400

    Added Opening time indicator, smooth scrolling to page and programmatically create a food card from an object, in an array

--- js/controller.js
+++ js/controller.js
@@ -1,14 +1,52 @@
 //// CONFIG ////
 
+//// VARIABLES ////
+
+// --Storing section IDs--
+
+const sectionAbout = document.querySelector('#section-about');
+const sectionMenu = document.querySelector('#section-menu');
+const sectionDelivery = document.querySelector('#section-delivery');
+
+// --Containers--
+
+const menuContainer = document.querySelector('.menu__list')
+
+// --Food Objects--
+
+const menuClassic = {
+    category: 'Beef Burgers',
+    name: 'This is new',
+    price: '14',
+    img: "css/img/menuimg.jpg",
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, facere!'
+}
+
+const menuClassic2 = {
+    category: 'Beef Burgers',
+    name: 'Also new',
+    price: '14',
+    img: "css/img/menuimg.jpg",
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, facere!'
+}
+
+const menu = [menuClassic, menuClassic2]
 
 //// BUTTONS ////
 
 // --Header--
 
-const btnNav = document.querySelectorAll('.nav-item__link');
-const btnHeader = document.querySelector('.btn');
+const btnToAbout = document.querySelectorAll('.btn-to-about');
+const btnToMenu = document.querySelectorAll('.btn-to-menu');
+const btnToDelivery = document.querySelectorAll('.btn-to-delivery');
 
+// --pairing buttons with section IDs
 
+const btnWithID = [
+    [btnToAbout, sectionAbout],
+    [btnToMenu, sectionMenu],
+    [btnToDelivery, sectionDelivery]
+]
 
 // HEADER_______________________________________________________________________
 
@@ -27,8 +65,8 @@ const curTime = {
     sec,
 }
 const restaurantSchedule = {
-    weekEnd: [5, 6],
-    openAt: 11,
+    weekEnd: [0, 1, 6],
+    openAt: 10,
     closeAt: 23,
 }
 
@@ -39,27 +77,77 @@ const getCurrentTime = function () {
     curTime.hours = curTime.date.getHours();
     curTime.min = curTime.date.getMinutes();
     curTime.sec = curTime.date.getSeconds();
-
-    setTimeout(getCurrentTime, 1000);
 }
 
 
-getCurrentTime();
-
 //// Sets the restaurant schedule color to green if the current time is within opening times. By default, it's red.
 const openRestaurant = function () {
+    if (curTime.weekDay === restaurantSchedule.weekEnd.indexOf(curTime.weekDay) &&
+        curTime.hours > restaurantSchedule.openAt &&
+        curTime.hours < restaurantSchedule.closeAt) {
+        document.documentElement.style.setProperty('--open-close', '#88ff00');
+        setTimeout(openRestaurant, 10 * 60 * 1000);
+        }
+
     if (curTime.weekDay !== restaurantSchedule.weekEnd.indexOf(curTime.weekDay) &&
         curTime.hours > restaurantSchedule.openAt &&
         curTime.hours < restaurantSchedule.closeAt) {
         document.documentElement.style.setProperty('--open-close', '#88ff00');
-        setTimeout(openRestaurant, 1000);
-        }
+        setTimeout(openRestaurant, 10 * 60 * 1000);
+    }
 }
 
-openRestaurant();
-
-const test = function() {
-    document.documentElement.style.setProperty('--open-close', '#ff9900');
+const init = function () {
+    getCurrentTime();
+    openRestaurant();
 }
 
-btnHeader.addEventListener('click', test)
+init();
+
+//// Adding smooth scrolling to all section buttons.
+btnWithID.forEach(arr => arr[0].forEach(btn => btn.addEventListener('click', function(e){
+    e.preventDefault();
+
+    const sectionCoords = arr[1].getBoundingClientRect();
+    window.scrollTo({
+        left: sectionCoords.left + window.pageXOffset,
+        top: sectionCoords.top + window.pageYOffset,
+        behavior: 'smooth',
+    })
+})))
+
+// MENU_________________________________________________________________________
+
+// Creating food card
+const createCard = function (meal) {
+
+    const cardMarkUp = `
+    <div class="menu__card">
+        <div class="menu__card__img-box">
+            <div class="menu__card__price">
+                <span class="menu__card__price--num">${meal.price}</span>
+                <span class="menu__card__price--cur">aed</span>
+            </div>
+        <img src="${meal.img}" alt="${meal.name}" class="menu__img">
+        </div>
+        <div class="menu__card__title">
+            ${meal.name}
+        </div>
+        <p class="menu__card__description">
+            ${meal.description}
+        </p>
+    </div>
+    `
+    menuContainer.insertAdjacentHTML('beforeend', cardMarkUp);
+}
+
+const clearHTML = function () {
+        // Clearing the HTML content first
+        menuContainer.innerHTML = '';
+}
+
+clearHTML();
+
+menu.forEach(meal => createCard(meal))
+
+// DELIVERY_____________________________________________________________________
