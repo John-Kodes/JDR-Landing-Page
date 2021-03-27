36073809f5de16a3affcc8bf23d012cd7a22e38b
John-Kodes <jdanielsemine@gmail.com>
2021-03-27 16:35:05 +0400

    fixed google maps button, added food cards information, added fries and drinks card

--- js/controller.js
+++ js/controller.js
@@ -5,12 +5,12 @@
 // ------FOOD OBJECTS------
 
 //// Beef burgers
-const friesNDrinks = {
+const friesNDrinksBeef = {
     category: 'beef burgers',
-    name: '+Fries & Drinks',
-    price: '9',
+    name: 'COMBO Burger',
+    price: '+9',
     img: "css/img/menuimg.jpg",
-    description: 'For every burger, you can pay 9 AED for fries and blue lagoon!'
+    description: 'Additional to get French Fries and Blue Lemonade Drink!'
 }
 
 const classic = {
@@ -18,7 +18,7 @@ const classic = {
     name: 'Classic',
     price: '14',
     img: "css/img/menuimg.jpg",
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, facere!'
+    description: 'Beef burger topped with JD Special Sauce, Cheese Sauce & Veggies (Onion, Tomato & Lettuce)'
 }
 
 const arabish = {
@@ -26,7 +26,7 @@ const arabish = {
     name: 'Arabish',
     price: '14',
     img: "css/img/menuimg.jpg",
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, facere!'
+    description: 'Beef burger topped with JD Garlic Sauce& Vegies (Onion, Tomato & Lettuce)'
 }
 
 const breakfast = {
@@ -34,7 +34,7 @@ const breakfast = {
     name: 'Breakfast',
     price: '16',
     img: "css/img/menuimg.jpg",
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, facere!'
+    description: 'Beef burger topped with Egg, Cheese Sauce , JD Special Sauce & Veggies (Onion, Tomato, Lettuce)'
 }
 
 const kilobite = {
@@ -42,7 +42,7 @@ const kilobite = {
     name: 'Kilobite',
     price: '16',
     img: "css/img/menuimg.jpg",
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, facere!'
+    description: 'Beef burger topped with Bacon, Cheese Sauce, JD Special Sauce & Veggies (Onion, Tomato, Lettuce)'
 }
 
 const megabite = {
@@ -50,15 +50,15 @@ const megabite = {
     name: 'Megabite',
     price: '27',
     img: "css/img/menuimg.jpg",
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, facere!'
+    description: 'Double beef burger topped with Bacon, Double Cheese, Cheese Sauce, JD Special Sauce & Veggies (Onion, Tomato, Lettuce)'
 }
 
 const gigabite = {
     category: 'beef burgers',
     name: 'Gigabite',
-    price: '36',
+    price: '37',
     img: "css/img/menuimg.jpg",
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, facere!'
+    description: 'Triple beef burger topped with Bacon, Double Cheese, Cheese Sauce, JD Special Sauce & Veggies (Onion, Tomato, Lettuce)'
 }
 
 const terabite = {
@@ -66,21 +66,94 @@ const terabite = {
     name: 'Terabite',
     price: '47',
     img: "css/img/menuimg.jpg",
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, facere!'
+    description: 'Quad beef burger topped with Bacon, Double Cheese, Cheese Sauce, JD Special Sauce & Veggies (Onion, Tomato, Lettuce)'
 }
 
-//// Chicken burgers
+const allBeefBurgers = [friesNDrinksBeef, classic, arabish, breakfast, kilobite, megabite, gigabite, terabite]
 
-// same thing
+//// Chicken burgers
+const friesNDrinksChicken = {
+    category: 'chicken burgers',
+    name: 'COMBO Burger',
+    price: '+9',
+    img: "css/img/menuimg.jpg",
+    description: 'Additional to get French Fries and Blue Lemonade Drink!'
+}
+
+const classicChicken = {
+    category: 'chicken burgers',
+    name: 'Classic',
+    price: '14',
+    img: "css/img/menuimg.jpg",
+    description: 'Beef burger topped with JD Special Sauce, Cheese Sauce & Veggies (Onion, Tomato & Lettuce)'
+}
+
+const arabishChicken = {
+    category: 'chicken burgers',
+    name: 'Arabish',
+    price: '14',
+    img: "css/img/menuimg.jpg",
+    description: 'Beef burger topped with JD Garlic Sauce& Vegies (Onion, Tomato & Lettuce)'
+}
+
+const breakfastChicken = {
+    category: 'chicken burgers',
+    name: 'Breakfast',
+    price: '16',
+    img: "css/img/menuimg.jpg",
+    description: 'Beef burger topped with Egg, Cheese Sauce , JD Special Sauce & Veggies (Onion, Tomato, Lettuce)'
+}
+
+const kilobiteChicken = {
+    category: 'chicken burgers',
+    name: 'Kilobite',
+    price: '16',
+    img: "css/img/menuimg.jpg",
+    description: 'Beef burger topped with Bacon, Cheese Sauce, JD Special Sauce & Veggies (Onion, Tomato, Lettuce)'
+}
+
+const megabiteChicken = {
+    category: 'chicken burgers',
+    name: 'Megabite',
+    price: '27',
+    img: "css/img/menuimg.jpg",
+    description: 'Double beef burger topped with Bacon, Double Cheese, Cheese Sauce, JD Special Sauce & Veggies (Onion, Tomato, Lettuce)'
+}
+
+const allChickenBurgers = [friesNDrinksChicken, classicChicken, arabishChicken, breakfastChicken, kilobiteChicken, megabiteChicken]
 
 //// Value Meals
+
+const bm = {
+    category: 'value meals',
+    name: 'Budget Meal',
+    price: '13',
+    img: "css/img/menuimg.jpg",
+    description: '2 piece chicken (leg & thigh) with rice, Gravy OR Garlic Sauce'
+}
+
+const bucket10 = {
+    category: 'value meals',
+    name: 'Bucket 10Pcs',
+    price: '??',
+    img: "css/img/menuimg.jpg",
+    description: '10 Pieces of chicken with 2 sauces of your choosing'
+}
+
+const bucket20 = {
+    category: 'value meals',
+    name: 'Bucket 20Pcs',
+    price: '??',
+    img: "css/img/menuimg.jpg",
+    description: '20 Pieces of chicken with 2 sauces of your choosing'
+}
 
 const vm1 = {
     category: 'value meals',
     name: 'VM 1',
     price: '27',
     img: "css/img/menuimg.jpg",
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, facere!'
+    description: 'Meal with 1 pc Beef burger steak topped with Gravy sauce, 02 pc broasted chicken with Garlic Sauce, double rice (dine-in only) & Blue Lemonade Drink'
 }
 
 const vm2 = {
@@ -88,7 +161,7 @@ const vm2 = {
     name: 'VM 2',
     price: '31',
     img: "css/img/menuimg.jpg",
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, facere!'
+    description: 'Meal with 1 pc classic burger, French Fries & 02 pc broasted chicken with Garlic Sauce & Blue Lemonade Drink'
 }
 
 const vm3 = {
@@ -96,7 +169,7 @@ const vm3 = {
     name: 'VM 3',
     price: '27',
     img: "css/img/menuimg.jpg",
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, facere!'
+    description: 'Meal with 02 pcs Beef burger steak topped with Gravy sauce, double rice (dine-in only) & Blue Lemonade Drink'
 }
 
 const vm4 = {
@@ -104,7 +177,7 @@ const vm4 = {
     name: 'VM 4',
     price: '25',
     img: "css/img/menuimg.jpg",
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, facere!'
+    description: 'Meal with 03 pcs Broasted Chicken with Gravy or Garlic Sauce, French Fries, Bun & Blue Lemonade Drink'
 }
 
 const vm5 = {
@@ -112,24 +185,182 @@ const vm5 = {
     name: 'VM 5',
     price: '25',
     img: "css/img/menuimg.jpg",
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, facere!'
+    description: 'Meal with 03 pcs Broasted Chicken with Gravy or Garlic Sauce, double rice (dine in only),  & Blue Lemonade Drink'
 }
 
 const vm6 = {
     category: 'value meals',
     name: 'VM 6',
-    price: '31',
+    price: '27',
     img: "css/img/menuimg.jpg",
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, facere!'
+    description: 'Meal with spaghetti, 02 pcs broasted Chicken and Blue Lemonade Drink'
 }
+
+const vm7 = {
+    category: 'value meals',
+    name: 'VM 7',
+    price: '27',
+    img: "css/img/menuimg.jpg",
+    description: 'Meal with 02 pcs Chicken burger steak topped with Gravy sauce, double rice (dine-in only) & Blue Lemonade Drink'
+}
+
+const allVM = [bm, bucket10, bucket20, vm1, vm2, vm3, vm4, vm5, vm6]
 
 //// Desert
 
+const notAvailable = {
+    category: 'desert',
+    name: 'nothin',
+    price: 'Free',
+    img: "css/img/menuimg.jpg",
+    description: 'Not offering any deserts at the moment'
+}
+
+const allDesert = [notAvailable]
+
 //// Drinks
+const blueLemonade = {
+    category: 'drinks',
+    name: 'Blue Lemonade',
+    price: '8',
+    img: "css/img/menuimg.jpg",
+    description: 'Blue lemonade'
+}
+
+const softDrink = {
+    category: 'drinks',
+    name: 'Soft Drinks',
+    price: '4',
+    img: "css/img/menuimg.jpg",
+    description: 'Pepsi, Diet Pepsi, Mirinda, 7up, Mountain Dew'
+}
+
+const waterS = {
+    category: 'drinks',
+    name: 'Water Small',
+    price: '2',
+    img: "css/img/menuimg.jpg",
+    description: ''
+}
+
+const waterL = {
+    category: 'drinks',
+    name: 'Water large',
+    price: '5',
+    img: "css/img/menuimg.jpg",
+    description: ''
+}
+
+const allDrinks = [blueLemonade, softDrink, waterS, waterL]
 
 //// Sides
+const rice = {
+    category: 'sides',
+    name: 'Rice',
+    price: '3.5',
+    img: "css/img/menuimg.jpg",
+    description: ''
+}
 
-const menu = [friesNDrinks, classic, arabish, breakfast, kilobite, megabite, gigabite, terabite, vm1, vm2, vm3, vm4, vm5, vm6]
+const friesS = {
+    category: 'sides',
+    name: 'Fries Small',
+    price: '6',
+    img: "css/img/menuimg.jpg",
+    description: ''
+}
+
+const friesL = {
+    category: 'sides',
+    name: 'Fries Large',
+    price: '10',
+    img: "css/img/menuimg.jpg",
+    description: ''
+}
+
+const garlicSauce = {
+    category: 'sides',
+    name: 'garlic Sauce',
+    price: '3',
+    img: "css/img/menuimg.jpg",
+    description: ''
+}
+
+const gravySauce = {
+    category: 'sides',
+    name: 'Gravy Sauce',
+    price: '3',
+    img: "css/img/menuimg.jpg",
+    description: ''
+}
+
+const bacon = {
+    category: 'sides',
+    name: 'Bacon',
+    price: '3',
+    img: "css/img/menuimg.jpg",
+    description: ''
+}
+
+const egg = {
+    category: 'sides',
+    name: 'Egg',
+    price: '3',
+    img: "css/img/menuimg.jpg",
+    description: ''
+}
+
+const cheeseSquare = {
+    category: 'sides',
+    name: 'Cheese Square',
+    price: '3',
+    img: "css/img/menuimg.jpg",
+    description: ''
+}
+
+const cheeseDip = {
+    category: 'sides',
+    name: 'Cheese Dip',
+    price: '3',
+    img: "css/img/menuimg.jpg",
+    description: ''
+}
+
+const JDSpecialSauce = {
+    category: 'sides',
+    name: 'JD Special Sauce',
+    price: '3',
+    img: "css/img/menuimg.jpg",
+    description: ''
+}
+
+const chipotle = {
+    category: 'sides',
+    name: 'Chipotle',
+    price: '3',
+    img: "css/img/menuimg.jpg",
+    description: ''
+}
+
+const ketchup = {
+    category: 'sides',
+    name: 'Ketchup',
+    price: '3',
+    img: "css/img/menuimg.jpg",
+    description: ''
+}
+
+const mayonaise = {
+    category: 'sides',
+    name: 'Mayonaise',
+    price: '3',
+    img: "css/img/menuimg.jpg",
+    description: ''
+}
+
+const allSides = [rice, friesS, friesL, garlicSauce, gravySauce, bacon, egg, cheeseSquare, cheeseDip, JDSpecialSauce, chipotle, ketchup, mayonaise]
+
+const menu = [...allBeefBurgers, ...allChickenBurgers, ...allVM, ...allDesert, ...allDrinks, ...allSides]
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -250,7 +481,7 @@ const createCard = function (meal) {
     if (meal.category === 'sides') slide = 5;
 
     const cardMarkUp = `
-    <div class="menu__card ${meal.name === '+Fries & Drinks' ? 'menu__card--special' : ''}">
+    <div class="menu__card ${meal.name === 'COMBO Burger' ? 'menu__card--special' : ''}">
         <div class="menu__card__img-box">
             <div class="menu__card__price">
                 <span class="menu__card__price--num">${meal.price}</span>
